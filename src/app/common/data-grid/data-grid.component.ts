92a29d05385ab18c78550d524804c92c50f1f42c
John Paxton <pax@speedingplanet.com>
2019-02-27 09:04:46 -0500

    Sorting works!

--- src/app/common/data-grid/data-grid.component.ts
+++ src/app/common/data-grid/data-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import * as _ from 'lodash';
+import { SortDirection } from 'src/app/payees/payee-types';
 
 interface DataGridConfig {
   columns: string[];
@@ -10,16 +11,19 @@ interface DataGridConfig {
   templateUrl: './data-grid.component.html',
   styleUrls: ['./data-grid.component.css']
 })
-export class DataGridComponent implements OnInit {
+export class DataGridComponent<T> implements OnInit {
+
+  lastSortField = '';
+  lastSortDirection = SortDirection.asc;
 
   @Input()
-  data: any[];
+  data: T[];
 
   @Input()
   config: DataGridConfig;
 
   @Output()
-  selectRow = new EventEmitter<any>();
+  selectRow = new EventEmitter<T>();
 
   constructor() { }
 
@@ -34,4 +38,28 @@ export class DataGridComponent implements OnInit {
     return _.startCase(column.split('.').slice(-1).join(''));
   }
 
+  getSortClass(field: string) {
+    if (field === this.lastSortField) {
+      return this.lastSortDirection === SortDirection.asc ? 'sort-asc' : 'sort-desc';
+    }
+
+    return 'sort-blank';
+  }
+
+  handleRowClick(item: T) {
+    console.log('You clicked on a row!');
+    this.selectRow.emit(item);
+  }
+
+  handleHeaderClick(field: string) {
+    let sortDirection = SortDirection.asc;
+
+    if (this.lastSortField === field && this.lastSortDirection === SortDirection.asc) {
+      sortDirection = SortDirection.desc;
+    }
+
+    this.data = _.orderBy(this.data, field, sortDirection);
+    this.lastSortDirection = sortDirection;
+    this.lastSortField = field;
+  }
 }
